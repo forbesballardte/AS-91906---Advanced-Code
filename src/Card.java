98177bd2fbe8a4a6b2550707d1b4dbccc81419ee
Te Mihinga <forbesballardte@wgc.school.nz>
2025-05-26 12:02:24 +1200

    added images and started caode for print and add

---
+++ src/Card.java
@@ -0,0 +1,43 @@
+/*
+ * Creating the object Card
+ * 
+ * @author Te Mihinga
+ * @version v.1     26/05/25
+ */
+
+import ecs100.UI;
+
+public class Card {
+
+    // instance Variables
+    private int id;
+    private String name;
+    private double money;
+    private String IMAGE;
+
+    // Constructor
+    public Card(int id, String name, double money, String IMAGE) {
+        this.id = id;
+        this.name = name;
+        this.money = money;
+        this.IMAGE = IMAGE;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getMoney() {
+        return this.money;
+    }
+
+    public String getImage() {
+        return this.IMAGE;
+    }
+
+    public void printBook() 
+    {
+        UI.printf("\nName: %s\nAuthor: %f", name, money);
+    }
+}
+
---
+++ src/Interface.java
@@ -0,0 +1,47 @@
+import java.util.HashMap;
+import java.util.Collection;
+
+import ecs100.*;
+/**
+ * description
+ * 
+ * @author Te Mihinga
+ * @version v.1 26/05/25
+ */
+public class Interface {
+    
+    //instance variables
+    private HashMap<Integer, Card> set;
+    private int currCardId;
+
+    public Interface() {
+        // initialise Hashmap
+        set = new HashMap<Integer, Card>();
+
+        // add cards to hashmap
+        Card exeggcute = new Card(1, "Exeggcute", 6.30, "Exeggcute");
+        Card mew = new Card(2, "Mew", 8.89, "Mew");
+        Card jigglypuff = new Card(3, "Jigglypuff", 11.66, "Jigglypuff");
+        Card munna = new Card(4, "Munna", 1.97, "Munna");
+
+        this.currCardId = 4;
+    }
+
+    public void addCard() {
+        // create a new Card from inputted information
+        String cardName = UI.askString("Enter Name of Card: ");
+        double cardMoney = UI.askDouble("Enter Monetary Vaue of Card: ");
+        String cardImage = UI.askString("Enter the file name of your image: ");
+        currCardId += 1;
+        Card newCard = new Card(currCardId, cardName, cardMoney, cardImage);
+        set.put(currCardId, newCard);
+        UI.println("Card added");
+        newCard.printCard();
+    }
+
+    public void findCard() {
+        String findName = UI.askString("Enter Name of Card: ");
+        double findMoney = UI.askDouble("Enter Monetary Vaue of Card: ");
+        String findImage = UI.askString("Enter the file name of your image: ");   
+    }
+}
